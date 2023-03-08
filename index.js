1c31abac9a0a0f7603cf7a77f99a7cf7d2232914
elborracho420 <e.baxter@dark-coin.io>
2023-03-08 13:17:40 -0600

    Set up object API for canvas so it can be drawn on.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
+//using c instead of context because this will be repeated a lot
+const c = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+console.log(c);
