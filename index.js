67121814c2bce692f8485ed8a38ced0d38a1766b
elborracho420 <e.baxter@dark-coin.io>
2023-03-09 10:20:47 -0600

    Moved player.draw()function to animate loop

--- index.js
+++ index.js
@@ -48,13 +48,13 @@ const y = canvas.height / 2;
 
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
 
 const projectiles = [];
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw();
     projectiles.forEach(projectile => {
         projectile.update();
     })
