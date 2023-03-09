d40ea147e32b676873fab00c7d977c53e0a0a5a8
elborracho420 <e.baxter@dark-coin.io>
2023-03-09 09:58:57 -0600

    Updated animate loop with clear function to fix projectile animation. This hid the player element.

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const projectiles = [];
 
 function animate() {
     requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
     projectiles.forEach(projectile => {
         projectile.update();
     })
