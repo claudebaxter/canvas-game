5919f3cc9f22b26b05542425d11dd6c7b662f887
elborracho420 <e.baxter@dark-coin.io>
2023-03-09 22:19:17 -0600

    Set enemy radius sizes from 4 to 30px.

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const enemies = [];
 
 function spawnEnemies() {
     setInterval(() => {
-        const radius = Math.random() * 30
+        const radius = Math.random() * (30 - 4) + 4
 
         let x 
         let y
